aa00d1bf0e793738fcde260b707ea678d33fe17f
Pablo Slavkin <pslavkin@disenioconingenio.com.ar>
2019-02-14 00:24:27 -0300

    listo directorios, nunca habia usado dirent

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
+#include <sys/types.h>
+#include <getopt.h>
 #include <stdio.h>
+#include <dirent.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 
-int main(void)
+typedef std::vector<std::string> String_Vector;
 
+void List_Dirs(const std::string& dir, String_Vector& files)
 {
-   printf("init\r\n");
+ DIR* dirp = opendir(dir.c_str());
+    struct dirent* dp;
+    while ((dp = readdir(dirp)) != NULL) {
+       files.push_back(dp->d_name);
+       std::cout << dp->d_name << std::endl;
+    }
+    closedir(dirp);
+}
+
+
+int main(int argc, char** argv)
+{
+   if(argc < 2 )
+      exit(1);
+   else {
+      std::string Dir;
+      String_Vector Files;
+      Dir = argv[1];
+      List_Dirs(Dir,Files);
+   }
    return 0;
 }
 
